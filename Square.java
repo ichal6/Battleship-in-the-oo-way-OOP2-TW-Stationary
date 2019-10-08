1591ec4ef9a9d97f59fa710885fcba846a2a8350
Magdalena Gola <turcza.magdalena@gmail.com>
2019-10-08 17:30:39 +0200

    Add  getIsShipPart

--- Square.java
+++ Square.java
@@ -2,10 +2,14 @@ public class Square {
     private boolean isShipPart;
 
     public String toString(){
-        String content = (isShipPart) ? "X" : " ";
+        String content = (isShipPart) ? "X" : "-";
         return content;
     }
     public void setIsShipPart(){
         isShipPart = true;
+    }
+
+    public boolean void getIsShipPart(){
+        return isShipPart;
     }
 }
