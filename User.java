f85330e67b9b71a506fe2c6b667c54f0fe1b1812
Magdalena Gola <turcza.magdalena@gmail.com>
2019-10-08 17:18:12 +0200

    Add isHuman filed, getIsHumanMethod and getRandomGuess method

---
+++ User.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class User
+{
+    private boolean isHuman;
+
+    public User(boolean isHuman)
+    {
+        this.isHuman = isHuman;
+    }
+
+    public boolean getIsHuman()
+    {
+        return isHuman;
+    }
+
+    public getRandomGuess()
+    {
+    Random rand = new Random();
+    int x = rand.nextInt(10); 
+    int y = rand.nextInt(10); 
+    }
+
+
+
+
+}
