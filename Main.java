124e71604ba365f2e49af5f55a0fa0bdc6098dd9
Magdalena Gola <turcza.magdalena@gmail.com>
2019-10-09 11:28:19 +0200

    Add Main

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 
 public class Main{
     public static void main(String[] args){
-        Ocean ocean 
+        Ocean ocean = new Ocean();
+        ocean.addShip(3, 6, 2, true);
+        ocean.printBoard();
 
 
     }
